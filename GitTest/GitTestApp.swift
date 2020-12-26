d174aa7f794bb26169ce8a5d387eeb0843dd0ccb
Nakanishi Toshihiko <binarhythm_bb@yahoo.co.jp>
2020-12-26 17:13:15 +0900

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Toshihiko Nakanishi on 2020/12/26.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
